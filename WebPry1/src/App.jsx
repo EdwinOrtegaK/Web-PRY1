0132f19fc762aeecd337b418b85210146bb52b65
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-04-28 17:15:20 -0600

    chore: New route added to CRUD page

--- WebPry1/src/App.jsx
+++ WebPry1/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AdminPage from './pages/AdminPage';
+import CrudPage from './pages/CrudPage';
 import './App.css';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/crud" element={<CrudPage />} />
       </Routes>
     </Router>
   );
