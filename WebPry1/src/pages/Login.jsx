6f93e8a0b1acdef22d3ab8d51e092cbed3fd53cb
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-04-28 23:10:30 -0600

    refactor: User authentication functions added

--- WebPry1/src/pages/Login.jsx
+++ WebPry1/src/pages/Login.jsx
@@ -1,20 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../autenticacion/AuthContext'
 import '../App.css';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { setAuthData } = useContext(AuthContext);
+  const history = useNavigate();
   const [isChecked, setIsChecked] = useState(false);
 
-  const handleToggle = () => setIsChecked(!isChecked);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      // Suponiendo que tienes una función para autenticar al usuario
+      const isAuthenticated = await authenticateUser(username, password);
+      if (isAuthenticated) {
+        setAuthData({ username });
+        navigate('/crud');
+      } else {
+        alert('Credenciales incorrectas');
+      }
+    } catch (error) {
+      console.error('Error durante el inicio de sesión:', error);
+    }
+  };
 
   return (
     <div className="main">
-      <input type="checkbox" id="chk" aria-hidden="true" checked={isChecked} onChange={handleToggle} />
       <div className="signup">
-      <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="chk">Login</label>
-          <input type="username" name="username" placeholder="Username" required />
-          <input type="password" name="pswd" placeholder="Password" required />
-          <button>Login</button>
+          <input 
+            type="text" 
+            name="username" 
+            placeholder="Username" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required 
+          />
+          <input 
+            type="password" 
+            name="password" 
+            placeholder="Password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required 
+          />
+          <button type="submit">Login</button>
         </form>
       </div>
     </div>
