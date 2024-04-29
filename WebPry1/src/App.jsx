9200cf9340327a2abec2da02bea8e850b45a86fc
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-04-28 23:09:06 -0600

    refactor: New routing format and CrudPage blocking

--- WebPry1/src/App.jsx
+++ WebPry1/src/App.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AdminPage from './pages/AdminPage';
-import CrudPage from './pages/CrudPage';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./autenticacion/AuthContext";
+import HomePage from "./pages/HomePage";
+import AdminPage from "./pages/AdminPage";
+import CrudPage from "./pages/CrudPage";
+import ProtectedRoute from "./autenticacion/ProtectedRoute";
+import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/crud" element={<CrudPage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route
+            path="/crud"
+            element={
+              <ProtectedRoute>
+                <CrudPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
