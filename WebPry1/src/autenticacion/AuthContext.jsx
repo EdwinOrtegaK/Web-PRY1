4f1ed698a9b2233a013643bcd22e3c4433e8bab5
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-05-06 19:21:58 -0600

    feat: Function to maintain the login

--- WebPry1/src/autenticacion/AuthContext.jsx
+++ WebPry1/src/autenticacion/AuthContext.jsx
@@ -2,12 +2,10 @@ import React, { createContext, useState, useContext } from 'react';
 
 export const AuthContext = createContext(null);
 
-
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const login = (userCredentials) => {
-    // Aquí podrías agregar tu lógica de autenticación y luego:
     setUser(userCredentials);
   };
 
