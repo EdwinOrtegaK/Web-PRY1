833f4c8a506283b0fc12bb5ff118714eda90e5e7
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-05-07 18:29:38 -0600

    feat: General use of the token on screens

--- WebPry1/src/autenticacion/AuthContext.jsx
+++ WebPry1/src/autenticacion/AuthContext.jsx
@@ -1,15 +1,40 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  const login = (userCredentials) => {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    /*
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        if (decoded && decoded.username) {
+          setUser({ username: decoded.username });
+        } else {
+          console.log('Token no válido o expirado');
+          localStorage.removeItem('token');
+          localStorage.setItem('isAuth', false)
+        }
+      } catch (e) {
+        console.log('Error decoding token:', e);
+        localStorage.removeItem('token');
+        localStorage.setItem('isAuth', false)
+      }
+    }
+    */
+  }, []);
+
+  const login = (userCredentials, token) => {
+    localStorage.setItem('token', token);
     setUser(userCredentials);
   };
 
   const logout = () => {
+    localStorage.removeItem('token');
     setUser(null);
   };
 
--- WebPry1/src/autenticacion/ProtectedRoute.jsx
+++ WebPry1/src/autenticacion/ProtectedRoute.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { Navigate, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
-const ProtectedRoute = ({ children, ...rest }) => {
-  const { user } = useAuth();
+const ProtectedRoute = ({ children }) => {
+  const { user, token } = useAuth();
+  const [isValid, setIsValid] = useState(false);
 
-  if (!user) {
-    return <Navigate to="/admin" replace />;
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await fetch('/api/verify-token', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        if (response.ok) {
+          setIsValid(true);
+        } else {
+          setIsValid(false);
+        }
+      } catch (error) {
+        console.error('Error verifying token:', error);
+        setIsValid(false);
+      }
+    };
+
+    if (token) {
+      verifyToken();
+    }
+  }, [token]);
+
+  if (!user || !isValid) {
+    return <Navigate to="/admin" />;
   }
 
   return children;
