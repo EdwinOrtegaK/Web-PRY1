b6c68040d4e8353f37dde2c92e5ad29d65b4a7d7
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-05-07 18:23:31 -0600

    feat: React Suspense for lazy loading and page lock formatting

--- WebPry1/src/App.jsx
+++ WebPry1/src/App.jsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./autenticacion/AuthContext";
-import HomePage from "./pages/HomePage";
-import AdminPage from "./pages/AdminPage";
-import CrudPage from "./pages/CrudPage";
-import ProtectedRoute from "./autenticacion/ProtectedRoute";
 import "./App.css";
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AdminPage = lazy(() => import("./pages/AdminPage"));
+const CrudPage = lazy(() => import("./pages/CrudPage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 const App = () => {
+  const [autenticado, setAutenticado] = React.useState(false);
+  React.useEffect(() => {
+    const auth = localStorage.getItem('token');
+    setAutenticado(!!auth);
+  }, []);
+  console.log(autenticado);
+
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route
-            path="/admin/crud"
-            element={
-              <ProtectedRoute>
-                <CrudPage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <Suspense fallback={<div className="blog-loader">Cargando...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route
+              path="/admin/crud"
+              element={autenticado ? <CrudPage /> : <ErrorPage />}
+            />
+          </Routes>
+        </Suspense>
       </Router>
     </AuthProvider>
   );
